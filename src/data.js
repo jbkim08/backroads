5bd30dee4e6888dd2fa86a0c1abe259a33e3b93d
JBKIM <drv98@naver.com>
2024-06-17 11:26:37 +0900

    서비스 동적 데이터로

--- src/data.js
+++ src/data.js
@@ -20,3 +20,25 @@ export const socialLinks = [
   { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
   { id: 4, href: "https://www.twitter.com", icon: "fab fa-instagram" },
 ];
+
+//서비스 데이터
+export const services = [
+  {
+    id: 1,
+    icon: "fas fa-wallet fa-fw",
+    title: "saving money",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.",
+  },
+  {
+    id: 2,
+    icon: "fas fa-tree fa-fw",
+    title: "endless hiking",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.",
+  },
+  {
+    id: 3,
+    icon: "fas fa-socks fa-fw",
+    title: "amazing comfort",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit Asperiores, officia.",
+  },
+];
