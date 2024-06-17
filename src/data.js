7f52816a251d6414fb9ddf3b0cfbdc700f127e9b
JBKIM <drv98@naver.com>
2024-06-17 10:49:06 +0900

    인스타 아이콘 추가

--- src/data.js
+++ src/data.js
@@ -18,4 +18,5 @@ export const socialLinks = [
   { id: 1, href: "https://www.twitter.com", icon: "fab fa-facebook" },
   { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
   { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
+  { id: 4, href: "https://www.twitter.com", icon: "fab fa-instagram" },
 ];
