801efdebb59a7758c9c27fbc4ac66be75e20ecac
JBKIM <drv98@naver.com>
2024-06-17 10:43:39 +0900

    메뉴와 아이콘 데이터로 수정

---
+++ src/data.js
@@ -0,0 +1,21 @@
+//투어 이미지들 링크
+// import tour1 from '/images/tour-1.jpeg';
+// import tour2 from '/images/tour-2.jpeg';
+// import tour3 from '/images/tour-3.jpeg';
+// import tour4 from '/images/tour-4.jpeg';
+
+//페이지 메뉴 링크
+export const pageLinks = [
+  { id: 1, href: "#home", text: "홈" },
+  { id: 2, href: "#about", text: "어바웃" },
+  { id: 3, href: "#services", text: "서비스" },
+  { id: 4, href: "#tours", text: "투어" },
+  { id: 5, href: "#contact", text: "연락처" },
+];
+
+//소셜 링크
+export const socialLinks = [
+  { id: 1, href: "https://www.twitter.com", icon: "fab fa-facebook" },
+  { id: 2, href: "https://www.twitter.com", icon: "fab fa-twitter" },
+  { id: 3, href: "https://www.twitter.com", icon: "fab fa-squarespace" },
+];
